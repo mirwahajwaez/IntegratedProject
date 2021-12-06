66077484306000f172bcb3cbcba7123e646467ab
mirwahajwaez <mirwahajwaez@gmail.com>
2021-12-06 18:59:46 +0100

    choose exam screen & student

---
+++ app/src/main/java/com/example/integratedproject/student/ChooseStudent.kt
@@ -0,0 +1,20 @@
+package com.example.integratedproject.student
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.integratedproject.R
+import android.graphics.drawable.Drawable
+import android.widget.Toast
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+
+class ChooseStudent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_student)
+
+
+
+    }
+}
