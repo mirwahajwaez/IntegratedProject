147a481d2207e7aa7c505d12241a0e32b2f10c51
Sander165 <63393720+Sander165@users.noreply.github.com>
2021-12-07 10:10:43 +0100

    Students spinner

--- app/src/main/java/com/example/integratedproject/student/ChooseStudent.kt
+++ app/src/main/java/com/example/integratedproject/student/ChooseStudent.kt
@@ -1,20 +1,54 @@
 package com.example.integratedproject.student
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.integratedproject.R
 import android.graphics.drawable.Drawable
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.annotation.DrawableRes
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.integratedproject.admin.AdminMainActivity
+import com.example.integratedproject.database.DatabaseHelper
 
-class ChooseStudent : AppCompatActivity() {
+
+class ChooseStudent : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    private var databaseHelper: DatabaseHelper? = null
+    private var students = ArrayList<String>()
+    private var check = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_student)
+        databaseHelper = DatabaseHelper(this)
 
-        
+        val spinner = findViewById<Spinner>(R.id.spinnerStudents)
+        spinner.setSelection(0,false)
+        spinner.onItemSelectedListener = this
+
+        students = databaseHelper!!.allStudents()
+        val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item, students)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+
 
     }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        if (++check > 1) {
+            intent = Intent(this, StudentMainActivity::class.java)
+            intent.putExtra("SELECTED_STUDENT", students[position])
+            startActivity(intent)
+        }
+
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
+
 }
