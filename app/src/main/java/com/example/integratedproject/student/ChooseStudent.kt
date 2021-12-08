e1e3cde0f8bbb33b4dd5949dc987442e103a300c
Sander165 <63393720+Sander165@users.noreply.github.com>
2021-12-08 10:47:45 +0100

    get long and lat

--- app/src/main/java/com/example/integratedproject/student/ChooseStudent.kt
+++ app/src/main/java/com/example/integratedproject/student/ChooseStudent.kt
@@ -1,5 +1,7 @@
 package com.example.integratedproject.student
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,6 +14,15 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.integratedproject.admin.AdminMainActivity
 import com.example.integratedproject.database.DatabaseHelper
+import android.location.LocationManager
+
+import android.content.pm.PackageManager
+import android.location.Location
+
+import androidx.core.app.ActivityCompat
+
+
+
 
 
 class ChooseStudent : AppCompatActivity(), AdapterView.OnItemSelectedListener {
@@ -41,6 +52,11 @@ class ChooseStudent : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         if (++check > 1) {
             intent = Intent(this, StudentMainActivity::class.java)
             intent.putExtra("SELECTED_STUDENT", students[position])
+            val location = getLocationWithCheckNetworkAndGPS(this).toString()
+            val longitude = location.split(',')[0].split(' ')[1]
+            val latitude = location.split(',')[1].split(' ')[0]
+            Toast.makeText(this, "$longitude", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "$latitude", Toast.LENGTH_LONG).show()
             startActivity(intent)
         }
 
@@ -50,5 +66,43 @@ class ChooseStudent : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         TODO("Not yet implemented")
     }
 
+    fun getLocationWithCheckNetworkAndGPS(mContext: Context): Location? {
+        val lm = (mContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager)
+        val isGpsEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        val isNetworkLocationEnabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        var networkLoacation: Location? = null
+        var gpsLocation: Location? = null
+        var finalLoc: Location? = null
+        if (isGpsEnabled) if (ActivityCompat.checkSelfPermission(
+                mContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                mContext,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return null
+        }
+        gpsLocation = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        if (isNetworkLocationEnabled) networkLoacation =
+            lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+        if (gpsLocation != null && networkLoacation != null) {
+
+            //smaller the number more accurate result will
+            return if (gpsLocation.getAccuracy() > networkLoacation.getAccuracy()) networkLoacation.also {
+                finalLoc = it
+            } else gpsLocation.also { finalLoc = it }
+        } else {
+            if (gpsLocation != null) {
+                return gpsLocation.also { finalLoc = it }
+            } else if (networkLoacation != null) {
+                return networkLoacation.also { finalLoc = it }
+            }
+        }
+        return finalLoc
+    }
+
 
 }
+
+
